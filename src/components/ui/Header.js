b74c718d50b2a83e5ad6fa715290cc3f85403ece
Phil <qft.gtr@gmail.com>
2024-04-12 19:36:11 +0800

    Update Header.js

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -9,7 +9,7 @@ export default function Header () {
     <Navbar fluid className='shadow-md px-4 sm:px-8 lg:px-12 dark:bg-white'>
       <Navbar.Brand href={PROJECT_HOMEPAGE} target='_blank'>
         <img src={PROJECT_ICON} className='mr-3 h-7' alt='' />
-        <div className='text-lg font-bold'>{BRIDGE_CHANNEL}</div>
+        <div className='text-black text-lg font-bold'>{BRIDGE_CHANNEL}</div>
       </Navbar.Brand>
       <ConnectButton pill />
     </Navbar>
