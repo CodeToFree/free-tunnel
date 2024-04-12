c7671e85663a933251166ba261654318a9955697
Phil <qft.gtr@gmail.com>
2024-04-13 00:48:27 +0800

    fix safe app

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,27 @@
 const nextConfig = {
   pageExtensions: ['jsx', 'tsx'],
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: '*',
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: 'GET',
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: 'X-Requested-With, content-type, Authorization',
+          },
+        ],
+      },
+    ]
+  }
 }
 
 export default nextConfig
--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -1,10 +1,12 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
+import { PROJECT_ICON } from '@/lib/const'
+
 export default function Document() {
   return (
     <Html lang='en'>
       <Head>
-        <link rel='icon' sizes='32x32' href='/favicon.png' />
+        <link rel='icon' sizes='32x32' href={PROJECT_ICON} />
       </Head>
       <body>
         <Main />
