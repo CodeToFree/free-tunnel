c51ea83cad32359b212673c1da8c77b2482924c8
Phil <qft.gtr@gmail.com>
2024-04-26 13:53:49 +0800

    fix refresh api

--- src/pages/api/v1/refresh/index.jsx
+++ src/pages/api/v1/refresh/index.jsx
@@ -5,12 +5,12 @@ import { BRIDGE_CHANNEL, CHAINS_FROM, CHAINS_TO } from '@/lib/const'
 import { parseRequest } from '@/lib/request'
 
 const events = {
-  '0xc7f505b2f371ae2175ee4913f4499e1f2633a7b5936321eed1cdaeb6115181d2': 'Initialized',
-  '0xbc7cd75a20ee27fd9adebab32041f755214dbc6bffa90cc0225b39da2e5c2d3b': 'Upgraded',
-  '0xf8ccb027dfcd135e000e9d45e6cc2d662578a8825d4c45b5e32e0adf67e79ec6': 'AdminTransferred',
-  '0xb707b889cced682704e0cf1e7335f22abdfdfe14d9db54a47a1b8ec4d42406ee': 'VaultTransferred',
-  '0x2bf05609716bc4b090ad0e99b47b91881c7517771259c625df05db7e9d8c8181': 'ProposerAdded',
-  '0x0782d5a983f4f99019eb2dc8dfe67419752a13dca743154e71029fc01318d055': 'TokenAdded',
+  // '0xc7f505b2f371ae2175ee4913f4499e1f2633a7b5936321eed1cdaeb6115181d2': 'Initialized',
+  // '0xbc7cd75a20ee27fd9adebab32041f755214dbc6bffa90cc0225b39da2e5c2d3b': 'Upgraded',
+  // '0xf8ccb027dfcd135e000e9d45e6cc2d662578a8825d4c45b5e32e0adf67e79ec6': 'AdminTransferred',
+  // '0xb707b889cced682704e0cf1e7335f22abdfdfe14d9db54a47a1b8ec4d42406ee': 'VaultTransferred',
+  // '0x2bf05609716bc4b090ad0e99b47b91881c7517771259c625df05db7e9d8c8181': 'ProposerAdded',
+  // '0x0782d5a983f4f99019eb2dc8dfe67419752a13dca743154e71029fc01318d055': 'TokenAdded',
   '0x56f55aead8b59181fdff5497b6e66dbf6e45dc38bc59023cd1eb5b5657a134fa': 'TokenLockProposed',
   '0x930a00540f7b6ebe716d5bc3cd6a963cfcc17295d86ea4a47c6ce7bcc55bf144': 'TokenLockExecuted',
   '0x0947081fc739b6bd9d04e8351b71b85c9da260db9da35c8f1e7b6a00e3239d4e': 'TokenLockCancelled',
@@ -54,12 +54,15 @@ async function refresh(chain) {
 
     reqs = reqs.concat(logs.map(log => {
       const [sig, param1, param2] = log.topics
-      const event = events[sig] || sig
+      const event = events[sig]
+      if (!event) {
+        return
+      }
       const reqId = param1?.substring(0, 38)
       const { fromChain, toChain } = parseRequest(reqId)
       const addr = param2 && ethers.utils.hexZeroPad(BigInt(param2), 20)
       return { event, hash: log.transactionHash, reqId, fromChain, toChain, addr }
-    }))
+    }).filter(Boolean))
 
     toBlock -= 1000
   }
