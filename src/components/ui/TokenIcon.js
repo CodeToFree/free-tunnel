94bb56435464f20e4d7ac6ac36f64db4cbc25b8f
Phil <qft.gtr@gmail.com>
2024-05-09 01:20:57 +0800

    Update TokenIcon.js

--- src/components/ui/TokenIcon.js
+++ src/components/ui/TokenIcon.js
@@ -27,10 +27,14 @@ export default function TokenIcon ({ size, token, className }) {
     return <img src='/tokens/bnb.png' className={_className} />
   } else if (token === 'polygon' || token === 'matic') {
     return <img src='/tokens/polygon.png' className={_className} />
+  } else if (token === 'linea') {
+    return <img src='/tokens/linea.png' className={_className} />
   } else if (token === 'mode') {
     return <img src='/tokens/mode.png' className={_className} />
   } else if (token === 'manta') {
     return <img src='/tokens/manta.png' className={_className} />
+  } else if (token === 'zklink') {
+    return <img src='/tokens/zklink.png' className={_className} />
   } else if (token === 'merl' || token === 'merlin') {
     return <img src='/tokens/merlin.png' className={_className} />
   } else if (token === 'b2') {
