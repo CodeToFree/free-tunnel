be90464c8deb686b3f742053618169074abf9d6f
Phil <qft.gtr@gmail.com>
2024-05-28 13:44:10 +0800

    Update index.js

--- src/lib/const/index.js
+++ src/lib/const/index.js
@@ -13,6 +13,9 @@ import b2 from './chains/b2.json'
 import bitlayer from './chains/bitlayer.json'
 import bevm from './chains/bevm.json'
 import bb from './chains/bb.json'
+import sepolia from './chains/sepolia.json'
+import merlin_testnet from './chains/merlin_testnet.json'
+import b2_testnet from './chains/b2_testnet.json'
 
 export const DARK_MODE = process.env.NEXT_PUBLIC_DARK_MODE
 export const TESTNET = process.env.NEXT_PUBLIC_TESTNET
@@ -27,7 +30,9 @@ export const ROLES = {
   Executor: 'executor', 
 }
 
-const chains = { eth, arb, bnb, polygon, linea, scroll, mode, manta, zklink, core, merlin, b2, bitlayer, bevm, bb }
+const chains = TESTNET
+  ? { sepolia, merlin_testnet, b2_testnet }
+  : { eth, arb, bnb, polygon, linea, scroll, mode, manta, zklink, core, merlin, b2, bitlayer, bevm, bb }
 
 export const CHAINS = Object.entries(chains).map(([id, c]) => ({ ...c, id }))
 export const PROPOSE_PERIOD = Number(process.env.NEXT_PUBLIC_PROPOSE_PERIOD || 86400 * 2)
