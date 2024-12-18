40ba53b11869277e48ebc418aed99df5e09a2cb5
Phil <qft.gtr@gmail.com>
2024-12-18 14:01:43 +0800

    Update TunnelBoringMachine.sol

--- contracts/TunnelBoringMachine.sol
+++ contracts/TunnelBoringMachine.sol
@@ -5,12 +5,15 @@ import "./Tunnel/TunnelContract.sol";
 
 contract TunnelBoringMachine {
     uint64 public immutable VERSION;
+    address public immutable HUB_ADDRESS;
 
     constructor(uint64 version) {
         VERSION = version;
+        HUB_ADDRESS = msg.sender;
     }
 
     function openNewTunnel(address hubAddress, string memory tunnelName, bool isLockMode) external returns (address tunnelAddress) {
+        require(msg.sender == HUB_ADDRESS, "Only for FreeTunnelHub");
         bytes memory bytecode = abi.encodePacked(type(TunnelContract).creationCode, abi.encode(VERSION, hubAddress, tunnelName, isLockMode));
         bytes32 salt = bytes32(0);
         assembly {
