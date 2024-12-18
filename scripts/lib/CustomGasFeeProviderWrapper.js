2987ed8dd998af3d270a09b7650e9840a9085325
Phil <qft.gtr@gmail.com>
2024-12-18 19:39:25 +0800

    Update CustomGasFeeProviderWrapper.js

--- scripts/lib/CustomGasFeeProviderWrapper.js
+++ scripts/lib/CustomGasFeeProviderWrapper.js
@@ -15,25 +15,24 @@ module.exports = class CustomGasFeeProviderWrapper extends EthersProviderWrapper
     const block = await this.getBlockWithTransactions('latest')
     const transactions = [...block.transactions]
     let offset = 0
-    while (transactions.length < 10) {
+    while (transactions.length < 20) {
       offset++
       transactions.push(...(await this.getBlockWithTransactions(block.number - offset)).transactions)
       if (offset > 10) {
-        transactions.push({})
         break
       }
     }
     const eip1559Transactions = transactions.filter(tx => tx.maxFeePerGas && tx.maxPriorityFeePerGas)
 
-    setTimeout(() => this._eip1559FeeData = undefined, 30_000)
+    setTimeout(() => this._eip1559FeeData = undefined, 60_000)
 
-    if (eip1559Transactions.length / transactions.length < 0.5) {
+    if (!transactions.length || eip1559Transactions.length / transactions.length < 0.5) {
       this._eip1559FeeData = null
       return
     }
 
     const sorted = eip1559Transactions.sort((tx1, tx2) => tx1.maxFeePerGas.lt(tx2.maxFeePerGas) ? -1 : 1)
-    const index = Math.ceil(sorted.length * 0.1)
+    const index = Math.ceil(sorted.length * 0.5)
     const { gasPrice, maxFeePerGas, maxPriorityFeePerGas } = sorted[index]
     this._eip1559FeeData = { gasPrice, maxFeePerGas, maxPriorityFeePerGas }
 
