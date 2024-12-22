774dd18c6f50e1c27fa54af88a0704a84835ef4b
Phil <qft.gtr@gmail.com>
2024-12-22 22:13:02 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -9,7 +9,7 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: '/api/:path*',
+        source: '/:path*',
         headers: [
           {
             key: 'Access-Control-Allow-Origin',
