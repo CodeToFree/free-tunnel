e21d4c9b6041ea2a049e338942bfcfacd6947d03
Phil <qft.gtr@gmail.com>
2025-02-07 16:26:48 +0800

    update rsk

--- scripts/deployHub.js
+++ scripts/deployHub.js
@@ -41,7 +41,7 @@ const { deployContract } = require('./lib')
 // 0x2d: ✅ corn 20241206
 // 0x2e: ✅ lisk 20241206
 // 0x2f: ✅ taker 20241220
-// 0x30: ✅ rsk 20241220
+// 0x30: ✅ rsk 20250105
 // 0xa0: (non-evm) sui
 // 0xf0: sepolia
 // 0xf1: merlin_testnet
