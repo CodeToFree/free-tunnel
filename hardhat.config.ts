b13077da5dc028ddb1ea9f6df7a2e4a452502d01
Phil <qft.gtr@gmail.com>
2025-03-22 11:23:53 +0800

    update rpc

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -85,7 +85,7 @@ const hardhatConfig = {
       url: 'https://arb1.arbitrum.io/rpc',
     },
     bnb: {
-      url: 'https://binance.llamarpc.com',
+      url: 'https://bsc-mainnet.nodereal.io/v1/64a9df0874fb4a93b9d0a3849de012d3',
     },
     polygon: {
       url: 'https://polygon-rpc.com',
