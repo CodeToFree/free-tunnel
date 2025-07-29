779ad410dae9f11550b8b44addf84064b93caf6f
EasonYou <yycanusher@126.com>
2025-07-29 12:46:41 +0800

    Feat/contracts (#28)
    * feat: remove nextjs app
    
    * fix: update env
    
    * fix: update chains
    
    ---------
    
    Co-authored-by: EasonYou <yycanusher@gmail.com>

--- src/components/ui/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Header } from './Header'
-export { default as AppContainer } from './AppContainer'
-export { default as TokenIcon } from './TokenIcon'
-export { default as PendingDisplay } from './PendingDisplay'
-export { default as PaginationButtons } from './PaginationButtons'
